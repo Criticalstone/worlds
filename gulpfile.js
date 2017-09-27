982a3bb766f6261f27962b99a96a6162d267a05b
Criticalstone <kevinhoog@gmail.com>
2017-09-27 20:39:22 +0200

    Fixes gulp and setup a structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,45 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var sequence = require('run-sequence');
 var connect = require('gulp-connect');
+var panini = require('panini');
 
 gulp.task('connect', function() {
     connect.server({
-        root: 'public',
+        root: 'build',
         livereload: true
     });
+});
+
+gulp.task('pages', function() {
+  gulp.src('src/pages/**/*.html')
+    .pipe(panini({
+      root: 'src/pages/',
+      layouts: 'src/layouts/',
+      partials: 'src/partials/',
+      helpers: 'src/helpers/',
+      data: 'src/data/'
+    }))
+    .pipe(gulp.dest('build'));
 });
 
 gulp.task('sass', function(){
     return gulp.src('./sass/**/*.scss')
         .pipe(sass( { errLogToConsole: true }))
-        .pipe(gulp.dest('./public/css'));
+        .pipe(gulp.dest('./build/css'));
 });
 
 gulp.task('livereload', function() {
-    gulp.src('./public/**/*')
-        .pipe(connect.reload());
+    sequence('sass', 'pages', 'reload'); 
 });
+
+gulp.task('reload', function() {
+    gulp.src('./build/**/*').pipe(connect.reload())
+})
 
 gulp.task('watch', function() {
-    gulp.watch('./sass/**/*.scss', ['sass']);
-    gulp.watch('./public/**/*', ['livereload']);
+    gulp.watch('./sass/**/*.scss', ['livereload']);
+    gulp.watch('./src/**/*', ['livereload']);
 });
 
-gulp.task('default', ['connect', 'watch', 'sass']);
+gulp.task('default', ['pages', 'connect', 'watch', 'sass']);
